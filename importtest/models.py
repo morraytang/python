efd302917397f5cadd974ca8808acab4bf5a0f95
morraytang <28226519@qq.com>
2019-04-21 16:35:04 +0800

    test import *

---
+++ importtest/models.py
@@ -0,0 +1,16 @@
+
+COUNT = 365
+
+
+class BaseModule :
+    def forword(self):
+        self.input = 3
+
+    def backward(self):
+        pass
+
+    def compute(self,a , b):
+        return a + b
+
+
+
---
+++ test.py
@@ -0,0 +1,6 @@
+from importtest.models import *
+#导入一个模块py的*， 是导入里面声明的所有变量，方法，类等
+
+basemodule = BaseModule()
+print(basemodule.compute(10,20))
+print("COUNT IS : {}".format(COUNT))
