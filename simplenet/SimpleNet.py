d57dfbb07a35409067d8f3781f5460471d6a51a3
morraytang <28226519@qq.com>
2019-04-23 17:51:26 +0800

    simplenet with acti

--- simplenet/SimpleNet.py
+++ simplenet/SimpleNet.py
@@ -5,9 +5,20 @@ from torch import nn
 class SimpleNet(nn.Module):
     def __init__(self,in_dim, h1_dim , h2_dim, out_dim):
         super(SimpleNet, self).__init__()
-        self.layer1 = nn.Linear(in_dim,h1_dim)
-        self.layer2 = nn.Linear(h1_dim,h2_dim)
-        self.layer3 = nn.Linear(h2_dim,out_dim)
+        # self.layer1 = nn.Linear(in_dim,h1_dim)
+        # self.layer2 = nn.Linear(h1_dim,h2_dim)
+        # self.layer3 = nn.Linear(h2_dim,out_dim)
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim,h1_dim),
+            nn.ReLU(True)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(h1_dim,h2_dim),
+            nn.ReLU(True)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(h2_dim,out_dim),
+        )
 
     def forward(self, x):
         x = self.layer1(x)
